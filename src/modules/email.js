877e8b353177eaaefde57c34886faaa2e1964e7c
Saahil <saahilattud@gmail.com>
2024-05-13 21:42:14 -0400

    better debug

--- src/modules/email.js
+++ src/modules/email.js
@@ -1,5 +1,6 @@
 // handle n8n module parsing
 const EmailClient = require('./email_client')
+let debug = false;
 /**
  *
  * Body Should consit of {isValidEmailRequest:true, email:[...], stamp: "DATE:00000", workflow: [...] }
@@ -7,7 +8,8 @@ const EmailClient = require('./email_client')
  * @returns
  */
 module.exports = (config, cb) => (req, res, next) => {
-  console.log(req.method, req.headers, config)
+  console.log(req.headers, req.body)
+  if(debug) return res.status(200).end()
   const isEmailRequest = req.method === 'POST' && req.body && req.body.isValidEmailRequest && req.headers['x-mail-header'] == config.mailsignkey
   if (config.shallNotPassIfNotValid && !isEmailRequest) {
     return res.status(400).json({ error: 'Not valid email signature, check ur body or auth.'})
