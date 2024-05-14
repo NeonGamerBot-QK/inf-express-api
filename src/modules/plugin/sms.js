ed4ae22b15dbf47992cc2beaee69d6586fa322c8
Saahil <saahilattud@gmail.com>
2024-05-13 22:26:28 -0400

    a

--- src/modules/plugin/sms.js
+++ src/modules/plugin/sms.js
@@ -1,10 +1,10 @@
 // plugin for ../email_client.js
 module.exports = class SmsPlugin {
   init (raw, me) {
-    if (raw.attachments[0].filename === 'text_0.txt') {
+    if (raw.attachments[0].fileName === 'text_0.txt') {
             // its a txt
       const att = raw.attachments.shift()
-      let str = att.content.toString()
+      let str = Buffer.from(att.data, 'base64').toString()
       me.body = str
     }
   }
