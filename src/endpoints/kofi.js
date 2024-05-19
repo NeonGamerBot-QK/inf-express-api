d0f73ea55ba5bc09778d50d9b7a00846654cedc0
Saahil <saahilattud@gmail.com>
2024-05-18 22:27:23 -0400

    e

--- src/endpoints/kofi.js
+++ src/endpoints/kofi.js
@@ -1,8 +1,29 @@
 // default template 
 module.exports = (router, db) => {
+    function updateItems(b) {
+        const currentItems = db.get('all_webhook')
+        currentItems.push(b)
+        db.set('all_webhook', currentItems)
+    }
+    if(!db.get('all_webhook'))db.set('all_webhook', [])
     router.post('/webhook', (req,res) => {
         const body = JSON.parse(req.body.data)
         console.log(body)
+        updateItems(body)
+        // check if its a shop one 
+        if(body.type == "Shop Order") {
+  // lets extract the identity info
+        // address will not be included becuase they should all be digital...
+        const email = body.email
+        const transactionId = body.kofi_transaction_id
+        // can pass an ID thru the message
+        const message = body.message
+        const items_bought = body.shop_items
+        // const currentItems = db.get('shop_store')
+// TODO:
+        }
+      
+        // const 
         // console.log(req.body)
         res.send(`200`)
     })
