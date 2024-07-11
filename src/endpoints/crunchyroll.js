493e7c0739f967129db403e73ad2652cd20619a0
Saahil <neon@saahild.com>
2024-07-11 19:06:41 -0400

    Work

--- src/endpoints/crunchyroll.js
+++ src/endpoints/crunchyroll.js
@@ -13,13 +13,13 @@ module.exports = (router, db) => {
         const visits = req.visits
         res.json({ visits })
     })
-    router.get('/comments/:epid/:epname', rateLimit({ windowMs: 1000, limit: 2 }), (req,res) => {
+    router.get('/comments/:epid/:epname', rateLimit({ windowMs: 1000, limit: 2 }), async (req,res) => {
         // db.get(`${req.params.epid}_${req.params.epname}`)
 res.json(
-    db.get(`${req.params.epid}_${req.params.epname}`) || []
+    await db.get(`${req.params.epid}_${req.params.epname}`) || []
 )
     })
-    router.post('/comments/:epid/:epname', rateLimit({ windowMs: 5000, limit: 3 }), (req,res) => {
+    router.post('/comments/:epid/:epname', rateLimit({ windowMs: 5000, limit: 3 }), async (req,res) => {
     const userId = req.headers['X-User-Id'] || req.headers['x-user-id']
     if(!userId) return res.status(400).json({ message: `User ID not found`})
     if(userId.split('-').length < 3) return res.status(400).json({ message: `User ID invalid\nOnly use this application via crunchyroll`})
@@ -30,14 +30,15 @@ if(!content) errors.push("Missing Property 'content'")
 if(typeof user_data !== 'object') errors.push("'user_data' is not an object")
     if(typeof content !== 'string') errors.push("'content' is not a string")
         if(errors.length > 0) return res.json({ message: `Multiple errors found`, errors })
-        const comments =     db.get(`${req.params.epid}_${req.params.epname}`) || []
+        const comments =     await db.get(`${req.params.epid}_${req.params.epname}`) || []
 comments.push({
     user_data, 
     content,
     userId,
     created_at: Date.now()
 })
-db.set(`${req.params.epid}_${req.params.epname}`, comments)
+await db.set(`${req.params.epid}_${req.params.epname}`, comments)
+res.status(201).json({ message: "OK NEW" })
     })
 }
 // module.exports.socket_handle = (socket,io,db) => {
