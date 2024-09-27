feaa8ed6aa069c08b125c7faaf100ee4a8bfe5f7
Saahil <neon@saahild.com>
2024-09-26 20:15:46 -0400

    wip: hackclub slack bot

---
+++ src/modules/slack-bot.js
@@ -0,0 +1,30 @@
+module.exports = class SlackBot {
+    constructor(token,verify) {
+        this.config = {
+            token,
+            verify
+        };
+        this.token = token;
+        this.verify = verify;
+    }
+    authMiddleware(req,res,next) {
+        if(req.body.token !== this.verify) {
+            res.status(401).send("Unauthorized");
+            return;
+        }
+        next();
+    }
+    eventMiddleware(req,res,next) {
+        req.event = req.body.event;
+        next();
+    }
+    makeRequest(url, method, data) {
+return fetch(`https://slack.com/api${url}`, {
+    headers: {
+        "Authorization": `${this.token}`,
+    },
+    method,
+    body: JSON.stringify(data),
+})
+        }
+        }
