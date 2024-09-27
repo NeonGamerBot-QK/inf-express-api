a7fcff852fc704f19a6d93837b4fd23f3122be81
Saahil <neon@saahild.com>
2024-09-26 20:19:41 -0400

    qf:e

--- src/endpoints/hackclub-hq-weather.js
+++ src/endpoints/hackclub-hq-weather.js
@@ -3,8 +3,8 @@ const SlackBot = require("../modules/slack-bot");
 // default template
 module.exports = (router, db) => {
   //router.get("/");
-  const slackInstance = new SlackBot( process.env.SLACK_BOT_TOKEN,process.env.SLACK_VERIFICATION_TOKEN);
-  router.get("/info", (req, res) => {
+  const slackInstance = new SlackBot( process.env.WEATHER_SLACK_BOT_SECRET,process.env.WEATHER_SLACK_VERIFICATION);
+  router.all("/info", (req, res) => {
     console.debug(req.body)
     res.send("wsp this is a wip atm");
   });
@@ -41,7 +41,9 @@ module.exports = (router, db) => {
     })
       .then(res => res.json())
   }
-
+  router.use((req,res,next) => {
+    res.send(`404`)
+  })
   router.use((err, req, res) => {
     console.error(err.stack);
     res.send(err.message);
