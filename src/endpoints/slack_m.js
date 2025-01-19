54ce45880c8fdab67a14271230377610ae525b27
Saahil <neon@saahild.com>
2025-01-18 22:19:18 -0500

    feat: get pronouns

--- src/endpoints/slack_m.js
+++ src/endpoints/slack_m.js
@@ -19,4 +19,34 @@ module.exports = (router, db) => {
       });
     }
   });
+  // uhh im just lazy
+  router.get("/pronouns", async (req, res) => {
+    const token = req.headers["authorization"];
+    if (!token) return res.status(401).json({ message: `no token` });
+
+    const uclient = new webclient.WebClient(token);
+    const fp = await db.get("cache_p" + req.query.user);
+    if (Date.now() - fp.expires < 0) {
+      return res.json({
+        message: "OK CACHE",
+        pronouns: fp.pronouns,
+      });
+    }
+    try {
+      const m = await uclient.users.profile.get({
+        user: req.query.user,
+      });
+      await db.set("cache_p" + req.query.user, {
+        expires: Date.now() + 60_000,
+        user: req.query.user,
+        pronouns: m.profile.pronouns,
+      });
+      res.json({ message: "OK", pronouns: m.profile.pronouns });
+    } catch (e) {
+      console.error(e);
+      res.status(500).json({
+        message: e.message,
+      });
+    }
+  });
 };
