1d037d8d52fac25b6bd5368a29137f2f66d0dc5a
Saahil <neon@saahild.com>
2025-01-23 22:06:30 -0500

    feat: irl auth

---
+++ src/endpoints/irl.js
@@ -0,0 +1,17 @@
+// default template 
+module.exports = (router, db) => {
+    router.use((req, res, next) => {
+        if (req.headers["authorization"] !== process.env.IMESSAGE_TOKEN)
+          return res.status(401).json({
+            invalid: true,
+            message: "Invalid token",
+          });
+        next();
+      });
+   // so pretty much. just static json data :0 (for this route)
+   router.get('/matrix-board', async (req,res) => {
+    res.json({ 
+        message: "h"
+    })
+   })
+}
