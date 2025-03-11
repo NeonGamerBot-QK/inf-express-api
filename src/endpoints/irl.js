bf06a9e7f74300e318ebcee83c0d3fe8c6f4bb48
Neon <neon@saahild.com>
2025-03-11 18:27:07 -0400

    f

--- src/endpoints/irl.js
+++ src/endpoints/irl.js
@@ -1,7 +1,7 @@
 // default template
 module.exports = (router, db) => {
   router.use((req, res, next) => {
-    if (req.headers["authorization"] !== process.env.IMESSAGE_TOKEN)
+    if (req.headers["authorization"] !== process.env.IMESSAGE_TOKEN || req.query.a !== process.env.IMESSAGE_TOKEN)
       return res.status(401).json({
         invalid: true,
         message: "Invalid token",
@@ -20,8 +20,10 @@ module.exports = (router, db) => {
     res.status(200).end()
   })
   router.get("/matrix/status", async (req, res) => {
+    const mybot_data = await fetch ("https://mybot.saahild.com/irl/matrix?a="+process.env.IMESSAGE_TOKEN).then(r=>r.json())
     res.json({
       is_sleeping: (await db.get("sleeping")) || false,
+      ...mybot_data
     });
   });
   router.get("/camera_alert", (req, res) => {
