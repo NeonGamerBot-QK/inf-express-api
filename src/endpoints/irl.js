18dc282096c356ee0e09c0b7268000b38025100f
Neon <neon@saahild.com>
2025-03-11 18:31:17 -0400

    fix: oops

--- src/endpoints/irl.js
+++ src/endpoints/irl.js
@@ -1,7 +1,10 @@
 // default template
 module.exports = (router, db) => {
   router.use((req, res, next) => {
-    if (req.headers["authorization"] !== process.env.IMESSAGE_TOKEN || req.query.a !== process.env.IMESSAGE_TOKEN)
+    if (
+      req.headers["authorization"] !== process.env.IMESSAGE_TOKEN ||
+      req.query.a !== process.env.IMESSAGE_TOKEN
+    )
       return res.status(401).json({
         invalid: true,
         message: "Invalid token",
@@ -14,16 +17,19 @@ module.exports = (router, db) => {
       message: "h",
     });
   });
-  router.post('/matrix/toggle_sleep',async (req, res) => {
-    const cv = await db.get("sleeping") || false,
-    await db.set("sleeping", !cv)
-    res.status(200).end()
-  })
+  router.post("/matrix/toggle_sleep", async (req, res) => {
+    const cv = (await db.get("sleeping")) || false;
+
+    await db.set("sleeping", !cv);
+    res.status(200).end();
+  });
   router.get("/matrix/status", async (req, res) => {
-    const mybot_data = await fetch ("https://mybot.saahild.com/irl/matrix?a="+process.env.IMESSAGE_TOKEN).then(r=>r.json())
+    const mybot_data = await fetch(
+      "https://mybot.saahild.com/irl/matrix?a=" + process.env.IMESSAGE_TOKEN
+    ).then((r) => r.json());
     res.json({
       is_sleeping: (await db.get("sleeping")) || false,
-      ...mybot_data
+      ...mybot_data,
     });
   });
   router.get("/camera_alert", (req, res) => {
